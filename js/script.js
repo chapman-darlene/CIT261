0e6635fd35ef41c1d6245c8664dc82bbd79164c1
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2019-01-29 18:15:40 -0500

    12292019_2
    Signed-off-by: chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -127,52 +127,100 @@ function map(){
     }
 }
   
-  function mapArray(){
-    var input1 = document.getElementById("num1").value -1;
-    var input2 = document.getElementById("num2").value -1;
-    document.getElementById("mapOutput").innerHTML = "Welcome to our Resort & Spa. Enter numbers to tour our beautiful resort";
+function mapArray(){
+  var input1 = document.getElementById("num1").value -1;
+  var input2 = document.getElementById("num2").value -1;
+  document.getElementById("mapOutput").innerHTML = "Welcome to our Resort & Spa. Enter numbers to tour our beautiful resort";
 
-    var mapArray = new Array(3);
-    for(i = 0; i < 3; i++){
-      mapArray[i] = new Array(3) 
-        for(j = 0; j< 3; j++){
-      }
+  var mapArray = new Array(3);
+  for(i = 0; i < 3; i++){
+    mapArray[i] = new Array(3) 
+      for(j = 0; j< 3; j++){
     }
-        mapArray [0][0] = "You are in the main lobby";
-        mapArray [0][1] = "You have reached the front desk";
-        mapArray [0][2] = "You have found the elevator";
-        mapArray [1][0] = "You are on the second floor. This is where the spa is located";
-        mapArray [1][1] = "The pool is located on the this floor";
-        mapArray [1][2] = "The gym is located on the this floor";
-        mapArray [2][0] = "This is guest room 300";
-        mapArray [2][1] = "This is guest room 301";
-        mapArray [2][2] = "You have reached your room, We hope you enjoy your stay";
+  }
+      mapArray [0][0] = "You are in the main lobby";
+      mapArray [0][1] = "You have reached the front desk";
+      mapArray [0][2] = "You have found the elevator";
+      mapArray [1][0] = "You are on the second floor. This is where the spa is located";
+      mapArray [1][1] = "The pool is located on the this floor";
+      mapArray [1][2] = "The gym is located on the this floor";
+      mapArray [2][0] = "This is guest room 300";
+      mapArray [2][1] = "This is guest room 301";
+      mapArray [2][2] = "You have reached your room, We hope you enjoy your stay";
 
-    document.getElementById("mapResult").innerHTML = mapArray[input1][input2]
+  document.getElementById("mapResult").innerHTML = mapArray[input1][input2]
 
-  } 
+} 
 
 function assocArray(){
-  var sport = document.querySelector("input[name='sport']:checked").nodeValue;
-  var sports = { "golf": "You will need golf clubs, a golf ball, a course with 18-holes", 
-              "tennis": "You will need a tennis racket, a tennis ball, and a court with net",
-              "basketball": "You will need a basketball hoop, a basketball, and a court",
-              "football" : "You will need a helmet and pads, a football, and a field"
-            }
-  <br>
-  {/* Use a for loop to access the information in the array */}
-  for(sport in sports) {
-    result = sports[sport];
-    document.getElementById("assocArray").innerHTML = value;
+ var input = document.querySelector("input[name='sport']:checked").value;
+ console.log(input);
+
+  var sports = { 
+    golf: "You will need golf clubs, golf balls, and a course with 18-holes",
+    tennis: "You will need a tennis racket, a tennis ball, and a court with net",
+    basketball: "You will need a basketball hoop, a basketball, and a court",
+    football: "You will need a helmet and pads, a football, and a field"
+  };
+
+  document.getElementById("assocArray").innerHTML = input + " = " + sports[input];
+  
+}
+
+var books = [
+  { ISBN: "9781420922530",
+    Title:"Hamlet", 
+    Author:"William Shakespeare", 
+    Description:"Considered one of Shakespeare's most rich and enduring plays, the depiction of its hero Hamlet as he vows to avenge the murder of his father by his brother Claudius is both powerful and complex. As Hamlet tries to find out the truth of the situation, his troubled relationship with his mother comes to the fore, as do the paradoxes in his personality. A play of carefully crafted conflict and tragedy, Shakespeare's intricate dialogue continues to fascinate audiences to this day.",
+    Published: "1603",
+    Rating: "4.5 stars"},
+
+  {ISBN: "9780199535569",
+  Title:"Pride and Prejudice", 
+  Author:"Jane Austen", 
+  Description:"Known to Jane Austin as \"her own darling child\", Pride and Prejudice is a brilliant work that compares to no other. It is a romantic clash between the opinionated Elizabeth Bennet, and her proud beau, Mr. Darcy. Austen's radiant wit sparkles as she weaves a story of the most superb comedy of Regency England manners.",
+  Published: "1813",
+  Rating: "5 stars"},
+
+  {ISBN: "9780141321103",
+  Title:"The Adventures of Tom Sawyer", 
+  Author:"Mark Twain", 
+  Description:"Tom Sawyer is sure to find trouble where the river leads him. On the banks of the Mississippi River, Tom and his friends seek out adventure at every turn.",
+  Published: "1876",
+  Rating: "4 stars"},
+
+  {ISBN: "9781853260391",
+  Title:"A Tale of Two Cities", 
+  Author:"Charles Dickens", 
+  Description:"'Liberty, equality, fraternity, or death; -- the last much the easiest to bestow, O Guilotine!' After eighteen years as a political prisoner in the Bastille, the ageing Doctor Manette is finally released and reunited with this daughter in England. From the tranquil roads of London, they are drawn against their will to the vengeful, bloodstained streets of Paris.",
+  Published: "1859",
+  Rating: "4 stars"},
+
+  {ISBN: "9781853260193",
+  Title:"Vanity Fair", 
+  Author:"William Makepeace Thackeray", 
+  Description:"A novel that chronicles the lives of two women who could not be more different: Becky Sharp, an orphan whose only resources are her vast ambitions, her native wit, and her loose morals; and her schoolmate, Amelia Sedley, a typically naive Victorian heroine, the pampered daughter of a wealthy family",
+  Published: "1859",
+  Rating: "4 stars"},
+
+] ;
+
+function selection(){
+  var input = document.getElementById("bookSelection").value;
+  var parsedFile = JSON.parse(input);
+  for (var i = 0; i < books.length; i++) {
+    var object = books[i];
+    for (var property in object) {      
+      if(object.ISBN == input){
+        if("ISBN" == String(property)){
+          document.getElementById("json_parse").innerHTML += "<hr>";
+        }        
+        document.getElementById("json_parse").innerHTML += property + ": " + object[property] + "<br>";       
+      }
+    }
   }
 }
 
-function assocMap() {
-  let input = document.getElementById("assocButton").value;
-
-  var resort = [
-    {
-
-    }
-  ]
+function json_stringify(){
+  document.getElementById("json_stringify").innerHTML = JSON.stringify(books);
 }
