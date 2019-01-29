9014286d391a13869bc87ad9d9bc0146dbeaa204
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2019-01-29 10:38:21 -0500

    12292019_1
    Signed-off-by: chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ function haystackValidator (){
   var input, numb;
   input = document.getElementById("haystack").value;
   numb = parseInt(input);
-  if(numb < 1 || numb > 10){
+  if((numb < 1 )|| (numb > 10)){
     document.getElementById("haystackResponse").innerHTML = "This is an invalid entry. <br>" +
     "Please try again.";
   }else {
@@ -81,7 +81,7 @@ function superHero(){
       case "batman":
       case "wonderWoman":
 
-      document.getElementById("comics").innerHTML = "Welcome to the Justice League!";
+      document.getElementById("comics").innerHTML = "This Super Hero is a member of the Justice League!";
       break;
 
       case "ironMan":
@@ -116,117 +116,63 @@ function dice(){
 //multi-dimensional array example
 function map(){
   var input1 = document.getElementById("num1").value;
-  var input2 = document.getElementById("num2").value;  
-  var map1 = parseInt(input1)-1;
-  var map2 = parseInt(input2)-1;
+  var input2 = document.getElementById("num2").value;
 
-  function inputValidator(a,b){
-    a = map1;
-    b = map2;
-    if((a < 0 || a > 8) || (b < 0 || b > 8 )){
+  a = parseInt(input1);
+  b = parseInt(input2);
+    if((a < 1 || a > 3) || (b < 1 || b > 3)){
       document.getElementById("mapOutput").innerHTML = "You have entered incorrect numbers."
+    } else{
+        mapArray();
     }
+}
+  
+  function mapArray(){
+    var input1 = document.getElementById("num1").value -1;
+    var input2 = document.getElementById("num2").value -1;
+    document.getElementById("mapOutput").innerHTML = "Welcome to our Resort & Spa. Enter numbers to tour our beautiful resort";
+
+    var mapArray = new Array(3);
+    for(i = 0; i < 3; i++){
+      mapArray[i] = new Array(3) 
+        for(j = 0; j< 3; j++){
+      }
+    }
+        mapArray [0][0] = "You are in the main lobby";
+        mapArray [0][1] = "You have reached the front desk";
+        mapArray [0][2] = "You have found the elevator";
+        mapArray [1][0] = "You are on the second floor. This is where the spa is located";
+        mapArray [1][1] = "The pool is located on the this floor";
+        mapArray [1][2] = "The gym is located on the this floor";
+        mapArray [2][0] = "This is guest room 300";
+        mapArray [2][1] = "This is guest room 301";
+        mapArray [2][2] = "You have reached your room, We hope you enjoy your stay";
+
+    document.getElementById("mapResult").innerHTML = mapArray[input1][input2]
+
+  } 
+
+function assocArray(){
+  var sport = document.querySelector("input[name='sport']:checked").nodeValue;
+  var sports = { "golf": "You will need golf clubs, a golf ball, a course with 18-holes", 
+              "tennis": "You will need a tennis racket, a tennis ball, and a court with net",
+              "basketball": "You will need a basketball hoop, a basketball, and a court",
+              "football" : "You will need a helmet and pads, a football, and a field"
+            }
+  <br>
+  {/* Use a for loop to access the information in the array */}
+  for(sport in sports) {
+    result = sports[sport];
+    document.getElementById("assocArray").innerHTML = value;
   }
-  
-  mapArray = new Array(3)
-
-  mapArray [0][0] = "You are in the main lobby";
-  mapArray [0][1] = "You have reached the front desk";
-  mapArray [0][2] = "You have found the elevator";
-  mapArray [1][0] = "You are on the first floor";
-  mapArray [1][1] = "The pool is located here";
-  mapArray [1][2] = "The gym is on this floor";
-  mapArray [2][0] = "This is guest room 200";
-  mapArray [2][1] = "This is guest room 201";
-  mapArray [2][2] = "You have reached your room";
-
-  document.getElementById("mapOutput").innerHTML = "Welcome to our Spa";
-  for(i = 0; i <= 3; i++){
-
-   for(j = 0; j <= 3; j++){
-     
-     document.getElementById("mapOutput").innerHTML = mapArray[i][j];
-   }
- }
-    
-  
 }
 
-  
+function assocMap() {
+  let input = document.getElementById("assocButton").value;
 
-
-/*  For loop with Break
-    var haystack = new Array()
-    haystack[17] = "Needle";
-
-    for(j=0; j<20; ++j){
-    if(haystack[j]=="Needle"){
-      document.write("<br>-Found at location: " + j);
-        break;
-    }else document.write(j + ", ");
-  }
-
-*/
-
-
-
-
-/*Switch Statement
-  switch(page)
+  var resort = [
     {
-    case "Home":
-      document.write("You selected Home");
-      break;
-    case "About":
-      document.write("You selected About");
-      break;
-    case "News":
-      document.write("You selected News");
-      break;
-    case "Login":
-      document.write("You selected Login");
-      break;
-    case "Links":
-      document.write("You selected Links");
-      break;
-  }
 
-*/
-
-/* Ternary operator
-  document.write(
-    a <=5 ?
-    "a is less than or equal to 5" :
-    "a is greater than 5";
-
-    size = a <=5 ? "short" : "long";
-) */
-
-/* document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('pkgPriceForm').addEventListener('submit', total);
-
-    var buttonTotal = document.getElementById('pkgTotal');
-}); */
-
-/*document.getElementById('pkgPriceForm').addEventListener('submit', total);
-var buttonTotal = document.getElementById('pkgTotal');
-
-function total(event) {
-    event.preventDefault();
-    var selectedItems = document.querySelectorAll('input[name="choice"]:checked');
-    var subtotal = 0;
-    selectedItems.forEach(function(i) {
-        subtotal += Number(i.value);
-    });
-
-}*/
-
-/*
-var selectedItems = document.querySelectorAll('input[name="choice"]:checked');
-var total = 0;
-selectedItems.forEach(function(i) {
-    total += Number(i.value);
-});
-
-document.getElementById("totalPrice").innerHTML = "The total package price is: $" + total;
-*/
+    }
+  ]
+}
